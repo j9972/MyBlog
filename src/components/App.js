d5cb483642332ce99183adb170fa151d5fbe9c46
jung su young <j9972@naver.com>
2021-03-25 13:40:31 +0900

    i make shape of LogIn page

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onChange = (event) => {
+    const {target: {name, value}} = event;
+    if(name === "email") {
+        setEmail(value);
+    } else if( name === "password") {
+        setPassword(value);
+    }
+    console.log(event.target.name); 
+  }
+
+  const onSubmit = async(e) => {
+    e.preventDefault();
+  }  
+
   return (
-    <div>hello let's start it</div>
+    <>
+      <form>
+        <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange}/>
+        <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}/>
+        <input type="submit" value="Log In" required />
+        {error && <span className="authError">{error}</span>}
+      </form>
+      <span onClick={toggleAccount}></span>
+    </>
   );
 }
 
