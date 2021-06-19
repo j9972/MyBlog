f05da3c6fca57b54319e622493ea78da87b0c9a9
jung su young <j9972@naver.com>
2021-06-19 17:14:10 +0900

    insert a few npm modules and audit fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,10 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
-import LoginPage from './pages/LoginPage';
-import HomePage from './pages/HomePage';
-import RegisterPage from './pages/RegisterPage';
-import CreatePost from './pages/CreatePost';
-import Post from './pages/Post';
-
 function App() {
   return (
     <>
-      <div className="navbar">
-        <div className="links">
-          <a href="/">Home Page</a> 
-          <a href="/register">Register Page</a> 
-          {/* 회원가입 */}
-          <a href="/login">Login Page</a> 
-          {/* 로그인 */}
-          <a href="/createpost">Create Post</a> 
-          {/* post 쓰는 페이지 */}
-        </div>
-      </div>
-
-      <Router>
-        <Route path="/" exact render={(props) => <HomePage />} /> 
-        <Route path="/register" exact render={(props) => <RegisterPage />} /> 
-        <Route path="/login" exact render={(props) => <LoginPage />} /> 
-        <Route path="/createpost" render={(props) => <CreatePost />} />
-        <Route path="/post/:postId" render={(props) => <Post />} />
-      </Router>
+      
     </>
   );
 }
--- client/src/pages/createpost.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function CreatePost() {
-    return (
-        <div className="CreatePost" >
-            hello my CreatePost
-        </div>
-    )
-}
