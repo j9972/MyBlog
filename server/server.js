a58f9b6877390dfa676d935f05690d69def06b20
jung su young <j9972@naver.com>
2021-06-19 17:20:31 +0900

    sequelize init

--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
 const express = require("express");
+const app = express();
+const PORT = 3001;
 const cors = require('cors');
-const mysql = require('mysql');
-const bodyParser = require("body-parser");
+
 const db = require("./config/db");
 
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-
-const app = express();
-const PORT = 3000;
 
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
