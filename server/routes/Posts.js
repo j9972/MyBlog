81dbcecc20f444a6cb5e862c9c4b8067e2248945
jung su young <j9972@naver.com>
2021-06-20 15:11:27 +0900

    id에 따라서 각 게시물을 볼 수 있도록 만듬

--- server/routes/Posts.js
+++ server/routes/Posts.js
@@ -1,18 +1,24 @@
+// 서버측 localhost url에 localhost:3001/posts 라고 ( posts 페이지 에서는 "/"이거여도 posts를 붙여줘야한다.)
+
 const express = require("express");
 const router = express.Router();
 const { Posts } = require("../models");
 
-// 서버측 localhost url에 localhost:3001/posts 라고 ( posts 페이지 에서는 "/"이거여도 posts를 붙여줘야한다.)
 router.get("/", async (req, res) => {
-    const listOfPosts = await Posts.findAll();
-    res.json(listOfPosts);
-  });
-
-router.post("/", async (req, res) => {
-    const post = req.body;
-    await Posts.create(post);
-    res.json(post);
+  const listOfPosts = await Posts.findAll();
+  res.json(listOfPosts);
 });
 
+router.get("/byId/:id", async (req, res) => {
+  const id = req.params.id;
+  const post = await Posts.findByPk(id);
+  res.json(post);
+});
+
+router.post("/", async (req, res) => {
+  const post = req.body;
+  await Posts.create(post);
+  res.json(post);
+});
 
 module.exports = router;
